5dbef93d9d1623b6e7f89a4043404e8a0f3c933d
Armando Rojano <armandorour@outlook.com>
2026-04-23 20:53:51 -0600

    Minor nice to have

--- components/About/About.tsx
+++ components/About/About.tsx
@@ -1,23 +1,24 @@
+import Container from '@/components/Container'
 import { aboutSection } from '@/data/about'
 
 export default function About() {
   return (
-    <section className="inline-flex w-full flex-col items-start justify-start gap-4 pb-50">
-      <p className="text-3xl font-normal font-serif italic text-primary">
-        {aboutSection.eyebrow}
-      </p>
-      <div className="text-5xl self-stretch font-normal font-sans leading-tight text-black">
-        {aboutSection.body.map((paragraph, index) => (
-          <p key={`${aboutSection.eyebrow}-${index + 1}`} className={index > 0 ? 'mt-8' : ''}>
-            {paragraph.split('\n').map((line, i) => (
-              <span key={i}>
-                {i > 0 && <br />}
-                {line}
-              </span>
-            ))}
-          </p>
-        ))}
-      </div>
+    <section className="w-full bg-black py-30">
+      <Container className="flex flex-col items-start gap-4">
+        <p className="text-3xl font-normal font-serif italic text-white">{aboutSection.eyebrow}</p>
+        <div className="self-stretch text-5xl font-normal font-sans leading-tight text-white">
+          {aboutSection.body.map((paragraph, index) => (
+            <p key={`${aboutSection.eyebrow}-${index + 1}`} className={index > 0 ? 'mt-8' : ''}>
+              {paragraph.split('\n').map((line, i) => (
+                <span key={i}>
+                  {i > 0 && <br />}
+                  {line}
+                </span>
+              ))}
+            </p>
+          ))}
+        </div>
+      </Container>
     </section>
   )
 }
